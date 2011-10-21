ce78dd7b349115ad5f893931b66b7aacc107ec56
koeart <koeart@zwoelfelf.org>
2011-10-22 00:55:35 +0200

    last -> docstring generation

--- pentabot.py
+++ pentabot.py
@@ -7,6 +7,15 @@ configfile = ".pentabot.conf"
 config = ConfigParser.RawConfigParser()
 config.read([configfile, configfile])
 
+config_help= {}
+config_help['lastrss']= "\n".join(dict(config.items('RSS')).keys())
+
+
+
+def format_help(fun):
+        fun.__doc__ = fun.__doc__.format(config_help)
+        return fun
+
 class pentaBot(JabberBot):
         """
         pentabot. It shall server you at your fingertips. And you shall serve him. With News for pentaradio.
@@ -23,19 +32,19 @@ class pentaBot(JabberBot):
                 '''ein echo fuer die welt'''
                 return args
 
+        @format_help
         @botcmd
         def last( self, mess, args):
                 '''
                 letzte Episode
                 Moegliche Eingaben:
-                pentacast
-                pentaradio
-                pentamusic
+                {lastrss}
                 '''
+
                 f = feedparser.parse('http://c3d2.de/' + args + '.xml').get('entries')[0]
                 return(
                 'Titel: ' + f.get('title') + '\n' +
-                'URL: ' + f.get('link') + '\n'
+                'URL:' + f.get('link') + '\n'
                 )
 
 #start Server
