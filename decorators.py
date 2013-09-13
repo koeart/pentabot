c60e0920734531c1894b4581db126ee46447d346
maloi <maloi>
2013-09-13 03:07:23 +0200

    reload botcommands dynamically

--- decorators.py
+++ decorators.py
@@ -2,10 +2,10 @@ from functools import wraps
 
 def ignore_msg_from_self(func):
     @wraps(func)
-    def decorator(self, msg, args):
+    def decorator(msg, args):
         if msg.getType() == "groupchat":
             if str(msg.getFrom()).split("/")[1] == 'PentaBot':
                 return
-        return func(self, msg, args)
+        return func(msg, args)
 
     return decorator
